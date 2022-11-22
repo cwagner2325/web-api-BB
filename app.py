d7a086c0997c617fdb1947323740ce8937b51947
Cayden Wagner <wagn2541@pacificu.edu>
2022-11-22 13:28:41 -0800

    Working on POST methods.

--- app.py
+++ app.py
@@ -21,28 +21,47 @@ items = [
 
 
 class getRequest(RequestHandler):
-    def get(self, id):
+    def get(self, id=None):
         global items
-        item = [item for item in items if item['id'] is int(id)]
-        self.write({'item': item})
+        if id is not None:
+            item = [item for item in items if item['id'] is int(id)]
+            self.write({'Output': item})
+        else:
+            self.write({"Output:\n": items})
 
 
-class requestHandlers(RequestHandler):
-    def post(self, _):
-        items.append(json.loads(self.request.body))
-        self.write({'message': 'new item added'})
+class postRequest(RequestHandler):
+    global items
+    print('reached')
+    def post(self, id=None):
+        if id is not None:
+            print('reached2')
+            user = [item for item in items if item['id'] is int(id)]
+            print( f'editing {user}')
+        else:
+            print('reached3')
+            user = {
+                "id": 5,
+                "name": "newAddition"
+            }
+            newItems = items.append(user)
+            items = newItems
+            self.write({"Output:\n": newItems})
 
-    def delete(self, id):
-        global items
-        new_items = [item for item in items if item['id'] is not int(id)]
-        items = new_items
-        self.write({'message': 'Item with id %s was deleted' % id})
+
+    # def delete(self, id):
+    #     global items
+    #     new_items = [item for item in items if item['id'] is not int(id)]
+    #     items = new_items
+    #     self.write({'message': 'Item with id %s was deleted' % id})
 
 
 def make_app():
     urls = [
-        (r"/GET/guid/([^/]+)?", getRequest),
-        (r"/api/item/([^/]+)?", requestHandlers)
+        ("/", getRequest),
+        (r"/guid/([^/]+)?", getRequest),
+        (r"/guid/([^/]+)?", postRequest),
+        (r"/guid/", postRequest),
     ]
     return Application(urls, debug=True)
 
