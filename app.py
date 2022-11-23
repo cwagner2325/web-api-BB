48ac9101739d8377ea7b302fb508b25677449dc8
Cayden Wagner <wagn2541@pacificu.edu>
2022-11-22 16:54:42 -0800

    Post working for user objects and get is almost working.

--- app.py
+++ app.py
@@ -11,8 +11,16 @@ class getRequest(RequestHandler):
     def get(self, id=None):
         global items
         if id is not None:
-            item = [item for item in items if item['id'] is int(id)]
-            self.write({'Output': item})
+            res = None
+            for item in items:
+                jsonItem = json.loads(item)
+                print(jsonItem['guid'])
+                print(id)
+                if str(jsonItem['guid']) is str(id):
+                    res = jsonItem
+                    print(res)
+
+            self.write({'Output': res})
         else:
             self.write({"Output:\n": items})
 
@@ -23,10 +31,9 @@ class postRequest(RequestHandler):
         global items
         user = User(user="Cayden Wagner")
         jsonUser = (json.dumps(user.__dict__))
-        
+
         items.append(jsonUser)
-        print(items)
-        self.write(f'Output: {items}')
+        self.write(f'Output: {jsonUser}')
 
 
     # def delete(self, id):
@@ -39,8 +46,8 @@ class postRequest(RequestHandler):
 def make_app():
     urls = [
         ("/", getRequest),
-        (r"/guid/([^/]+)?", getRequest),
-        (r"/guid/([^/]+)?", postRequest),
+        (r"/guid/[({]?[a-fA-F0-9]{8}[-]?([a-fA-F0-9]{4}[-]?){3}[a-fA-F0-9]{12}[})]?", getRequest),
+        (r"/guid/[({]?[a-fA-F0-9]{32}[})]?", postRequest),
         (r"/guid", postRequest),
     ]
     return Application(urls, debug=True)
